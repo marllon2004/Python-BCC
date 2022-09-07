a63354be8c96b7313cecaad3acd94f9698c11b85
Marllon Araujo <50830455+marllon2004@users.noreply.github.com>
2022-09-07 19:18:06 -0300

    Delete prova.py

--- prova.py
+++
@@ -1,8 +0,0 @@
-x1=float(input("informe o valor de x1: "))
-y1=float(input("informe o valor de y1: "))
-x2=float(input("informe o valor de x2: "))
-y2=float(input("informe o valor de y2: "))
-
-dist=((x2-x1)**2 + (y2-y1)**2) **0.5
-
-print(f'distancia: {dist:.4f}')
