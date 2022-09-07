2fea12d387576ff7f8fb73dac9f52cd5c72d7b18
Marllon Araujo <50830455+marllon2004@users.noreply.github.com>
2022-09-07 19:16:15 -0300

    Delete lista1 directory

--- lista1/ex002.py
+++
@@ -1,9 +0,0 @@
-#Fazer um programa para receber 3 valores inteiros do usuário e mostrar a sua média
-
-n1 = int(input("Informe a 1° nota: "))
-n2 = int(input("Informe a 2° nota: "))
-n3 = int(input("Informe a 3° nota: "))
-
-media = (n1 + n2 + n3) / 3
-
-print(f'A média entre {n1}, {n2} e {n3} é {media}')
--- lista1/ex008.py
+++
@@ -1,8 +0,0 @@
-#Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado pelo usuário, assim como a quantidade de dias pelos quais o carro foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$ 60 por dia e R$ 0,15 por km rodado
-
-km = float(input("Quantos kilometros percorridos? "))
-dias = int(input("Quantos dias o carro foi alugado? "))
-
-preco = (dias * 60) + (km * 0.15)
-
-print(f'O total a pagar é {preco}')
